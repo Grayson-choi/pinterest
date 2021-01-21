1d3cb47e4f19897e25365c8bddaf3a5e87943f6c
Grayson-choi <784wldnd@gmail.com>
2021-01-21 22:03:45 +0900

    profile app 완료 & get_success_url 완료

--- pinterest/profileapp/decorators.py
+++ pinterest/profileapp/decorators.py
@@ -5,8 +5,8 @@ from profileapp.models import Profile
 
 def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        user = Profile.objects.get(pk=kwargs['pk'])
-        if not user == request.user:
+        profile = Profile.objects.get(pk=kwargs['pk'])
+        if not profile.user == request.user:
             return HttpResponseForbidden()
         return func(request, *args, **kwargs)
     return decorated
--- pinterest/profileapp/views.py
+++ pinterest/profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -15,7 +15,8 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url은 pk를 넘겨줄 수가 없기 때문에 get_success_url을 사용한다.
+    # success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -25,6 +26,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -32,5 +36,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
