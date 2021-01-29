8f8999f37329baa9625aae4b59ac27816850fae0
Grayson-choi <784wldnd@gmail.com>
2021-01-29 17:27:17 +0900

    MultipleObjectMixin을 통한 ProjectApp 마무리

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -13,6 +13,8 @@ from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+from articleapp.models import Article
+
 has_ownership = [account_ownership_required, login_required]
 
 @login_required
@@ -40,6 +42,12 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 @method_decorator(has_ownership, 'get')
