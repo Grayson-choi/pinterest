d51dbe2d9c0c3fad717d9d37fd8334f15ce67eb4
Grayson-choi <784wldnd@gmail.com>
2021-02-01 18:41:44 +0900

    간단한 코드 수정 및 material icon 등록

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -11,24 +11,11 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
+
 
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
-
-@login_required
-def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        # 그냥 render를 쓰면 새로고침 할때마다 데이터가 저장되게됨 이를 방지하기 위해 Httpresponse Redirect를 사
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
--- pinterest/articleapp/forms.py
+++ pinterest/articleapp/forms.py
@@ -8,9 +8,9 @@ from projectapp.models import Project
 
 class ArticleCreationForm(ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
-                                                           'style': 'height: auto;'
-                                                           }))
+                                                           'style': 'height: auto;'}))
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
